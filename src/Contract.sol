d79de90303d092fd392f4861c5274e8b8ca954b9
cube <cubetv228@gmail.com>
2022-10-09 19:25:20 +0300

    added functions add / sub

--- src/Contract.sol
+++ src/Contract.sol
@@ -3,4 +3,17 @@ pragma solidity ^0.8.13;
 
 contract Contract {
     uint256 x = 0;
+
+    function add(uint256 y) external{
+        x += y;
+        emit Add(x, y);
+    }
+
+    function sub(uint256 y) external{
+        x -= y;
+        emit Sub(x, y);
+    }
+
+    event Add(uint256 x, uint256 y);
+    event Sub(uint256 x, uint256 y);
 }
