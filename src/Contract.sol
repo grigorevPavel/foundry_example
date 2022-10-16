aad80b74367ac7052fc21e7bfa00da228fd03343
Arina <Arina@MacBook-Air-Arina.local>
2022-10-16 12:18:59 +0300

    test

--- src/Contract.sol
+++ src/Contract.sol
@@ -2,18 +2,18 @@
 pragma solidity ^0.8.13;
 
 contract Contract {
-    uint256 x = 0;
+    int256 public x = 0;
 
-    function add(uint256 y) external{
+    function add(int256 y) external{
         x += y;
         emit Add(x, y);
     }
 
-    function sub(uint256 y) external{
+    function sub(int256 y) external{
         x -= y;
         emit Sub(x, y);
     }
 
-    event Add(uint256 x, uint256 y);
-    event Sub(uint256 x, uint256 y);
+    event Add(int256 x, int256 y);
+    event Sub(int256 x, int256 y);
 }
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,32 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "src/Contract.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
+    Contract testContract;
+    function setUp() public {        
+        testContract = new Contract();
+    }
 
     function testExample() public {
-        assertTrue(true);
+        bool x = true;
+        assertTrue(x);
     }
+    function testAdd() public {
+        int y=196;
+        int256 oldX = testContract.x();
+        testContract.add(y);
+        int256 newX = testContract.x();
+        assertEq(newX - oldX, y);
+        
+    }
+    function testSub() public {
+        int y=196;
+        int256 oldX = testContract.x();
+        testContract.sub(y);
+        int256 newX = testContract.x();
+        assertEq(newX- oldX, -y);
+    }
+
 }
